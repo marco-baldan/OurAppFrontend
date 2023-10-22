1c158bd7b6704313b2858e5f3716781cbf4f6f0b
marco-acn <marco.baldan@accenture.com>
2023-10-22 20:44:37 +0100

    okay, back end doesn't belong there

--- backend/db.js
+++
@@ -1,8 +0,0 @@
-import mysql from 'mysql2/promise';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-const connection = await mysql.createConnection(process.env.DATABASE_URL);
-
-export default connection;
